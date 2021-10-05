94486e9b5949655f0a2c80bdb8d5fa4eda0c888f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-05 14:12:34 -0500

    Using schema

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express"
+import schema from "./schema"
+import { graphqlHTTP } from "express-graphql"
 
 const app = express()
 
@@ -6,7 +8,18 @@ app.get("/", (req, res) => {
   res.send("GraphQL server")
 })
 
+const root = { hello: () => "Hi, I'm Ayaan" }
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+)
+
 const port = 8080
 app.listen(port, () => {
-  console.log(`Server running on port ${port}: http://localhost:8080`)
+  console.log(`Server running on port ${port}: http://localhost:8080/graphql`)
 })
