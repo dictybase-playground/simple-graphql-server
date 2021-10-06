bc09fc663deb1ee32a73a1b359c6ae5e0424fd11
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-06 13:03:37 -0500

    Using cors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from "express"
 import schema from "./schema"
 import { graphqlHTTP } from "express-graphql"
 import resolvers from "./resolvers"
+import cors from "cors"
 
 const app = express()
+
+app.use(cors())
 
 app.get("/", (_, res) => {
   res.redirect("/graphql")
